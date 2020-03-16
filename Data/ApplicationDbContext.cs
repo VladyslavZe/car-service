ec12250a4ef5514126f49c67e332adb8b1c45365
VladyslavZe <vladyslavze@tahometer.com>
2020-03-16 17:16:51 +0200

    created OredrItem model

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -10,12 +10,13 @@ using System.Threading.Tasks;
 
 namespace car_service.Data
 {
-    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+  public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+  {
+    public ApplicationDbContext(
+        DbContextOptions options,
+        IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
     {
-        public ApplicationDbContext(
-            DbContextOptions options,
-            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
-        {
-        }
     }
+
+  }
 }
